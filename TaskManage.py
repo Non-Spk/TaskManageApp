ad7c15d468c9b9e8227d47774f04c73957463540
supakpong <149934366+Non-Spk@users.noreply.github.com>
2025-04-22 22:21:49 +0700

    [Add] Implement save and load tasks using JSON file for task persistence

--- TaskManage.py
+++ TaskManage.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import uuid
+import json
 class Task:
     def __init__(self, title: str, description: str, due_date: str, completed=False):
         self.id = self.generate_id()
@@ -25,10 +26,12 @@ class Task:
     
 
 class TaskManager:
-    def __init__(self):
+    def __init__(self, json_file="tasks.json"):
         self.tasks = []
+        self.json_file = json_file
+        self.load_tasks_from_json()
 
-    def add_task(self, task: Task):
+    def add_task(self, task):
         self.tasks.append(task)
 
     def mark_as_completed(self, task_id):
@@ -38,7 +41,7 @@ class TaskManager:
                 print(f"Task ID {task_id} has been marked as completed.")
                 return
         print(f"Task ID {task_id} not found.")
-
+    
     def delete_task(self, task_id):
         for task in self.tasks:
             if task['id'] == task_id:
@@ -47,13 +50,29 @@ class TaskManager:
                 return
         print(f"Task ID {task_id} not found.")
 
+    def save_tasks_to_json(self):
+        with open(self.json_file, 'w') as file:
+            json.dump([task for task in self.tasks], file, default=str, indent=4)
+            print(f"Tasks have been saved to {self.json_file}")
+
+    def load_tasks_from_json(self):
+        try:
+            with open(self.json_file, 'r') as file:
+                data = json.load(file)
+                for task_data in data:
+                    task_data['due_date'] = datetime.strptime(task_data['due_date'], "%Y-%m-%d").date()  # Convert string back to date object
+                    self.tasks.append(task_data)
+                print(f"Tasks have been loaded from {self.json_file}")
+        except FileNotFoundError:
+            print(f"{self.json_file} not found. Starting with an empty task list.")
+
     def display_task(self):
         print('not Completed:')
         for task in self.tasks:
-            if not task.completed:
-                print(f"Task ID: {task.id}, Title: {task.title}, Description: {task.description}, Due Date: {task.due_date}")
+            if not task['completed']:
+                print(f"Task ID: {task['id']}, Title: {task['title']}, Description: {task['description']}, Due Date: {task['due_date']}")
 
         print('Completed:')
         for task in self.tasks:
-            if task.completed:
-                print(f"Task ID: {task.id}, Title: {task.title}, Description: {task.description}, Due Date: {task.due_date}")
+            if task['completed']:
+                print(f"Task ID: {task['id']}, Title: {task['title']}, Description: {task['description']}, Due Date: {task['due_date']}")
